03568787d3f2664a0c9cfff690506361e3d23fc5
cleghorn-riddingsIt <cleghorn-riddingsIt@users.noreply.github.com>
2024-03-17 17:05:07 +0000

    small changes

--- item.py
+++ item.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from enum import Enum
 
 
+#TODO: This needs to be configured in the database. Hardcoding workitem types is not a good idea
 class workItemType(Enum):
     Request=1
     Initiative=2
@@ -15,6 +16,7 @@ class workItemType(Enum):
     Bug=9
     Issue=10
 
+#all plannable objects will have a time span property, both a planned and actual start/end date. UTC time will be used for all dates
 @dataclass
 class time_span:
     start: datetime
@@ -22,8 +24,17 @@ class time_span:
     planned_start: datetime
     planned_end: datetime
     
+@dataclass
+class workFlowSteps:
+    id: int
+    name: str
     
-    
+
+#A work flow object is an indiaction of the state of that object(for example, a work item might be New, refining, on hold etc). These workflows should be configurable
+@dataclass
+class workflow:
+    id: int
+    name: str
     
 
 @dataclass
@@ -32,5 +43,7 @@ class workItem:
     title: str
     description: str
     workItem: workItemType
+    schedule: time_span
+    
 
    
